498398b05eb5a18557cda44684639385da3e2e84
nguyendongminhtri <nguyendongminhtri29092014@gmail.com>
2024-01-15 01:33:49 +0700

    master thay doi

--- Main.java
+++ Main.java
@@ -8,5 +8,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("CODE THUOC MASTER");
         System.out.println("CODE THUOC DEV");
+        System.out.println("MASTER THAY DOI");
     }
 }
